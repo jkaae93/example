8645b6a08f339850c8f1724628341be9ce7836b2
Celine Jeon <jkaae93@gmail.com>
2024-11-06 18:37:09 +0900

    Create openstage_example.dart

---
+++ openstage_example.dart
@@ -0,0 +1,66 @@
+void main() {
+  Uri deeplink = Uri.parse('flutter://community/board/view?id=3043');
+
+  switch (deeplink.host) {
+    case 'community':
+      _moveCommunity(deeplink.pathSegments, deeplink.queryParametersAll);
+      break;
+    case 'shopping':
+      // NOTE: Do something this.
+      break;
+    default:
+      print('Handled Exception');
+  }
+}
+
+void _moveCommunity(List<String> paths, Map<String, dynamic> params) {
+  switch (paths.first) {
+    case 'board':
+      _defaultAction(
+        paths[1],
+        params,
+        doView: (id) {
+          print('Do board view ${id}');
+        },
+        doCreate: (id) {
+          print('Do board Create ${id}');
+        },
+      );
+      break;
+    case 'event':
+      _defaultAction(
+        paths[1],
+        params,
+        doView: (id) {
+          print('Do event view ${id}');
+        },
+        doCreate: (id) {
+          print('Do event create ${id}');
+        },
+      );
+  }
+}
+
+void _defaultAction(
+  String path,
+  Map<String, dynamic> params, {
+  required Function(int) doView,
+  required Function(int) doCreate,
+}) {
+  switch (path) {
+    case 'view':
+      doView(_getId(params));
+      break;
+    case 'write':
+      doCreate(_getId(params));
+      break;
+  }
+}
+
+int _getId(Map<String, dynamic> params) {
+  try {
+    return int.parse(params['id'] ?? '0');
+  } catch (e) {
+    return 0;
+  }
+}
